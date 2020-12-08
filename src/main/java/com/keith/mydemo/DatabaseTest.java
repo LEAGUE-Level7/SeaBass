6aee8881b9f202bdef25a2d2de3b58430e9bad5b
ArnavPalkhiwala <palkhiwala.arnav@gmail.com>
2020-12-07 17:06:18 -0800

    update

--- src/main/java/com/keith/mydemo/DatabaseTest.java
+++ src/main/java/com/keith/mydemo/DatabaseTest.java
@@ -10,34 +10,34 @@ public class DatabaseTest {
 		        connection = DriverManager
 		           .getConnection("jdbc:postgresql://localhost:5432/seabassdox",
 		           "postgres", "asdf");
-		        System.out.println("Connected to database!");
-		     } catch (Exception e) {
-		        e.printStackTrace();
-		        System.err.println(e.getClass().getName()+": "+e.getMessage());
-		        System.err.println("FAILED TO CONNECT TO POSTGRES DATABASE");
-		        connection = null;
-		     }
-	     
-	}
-
-	 public static String getAllData() {
-		 if(connection == null) {
-			 return "database error";
-		 }
-        Statement statement;
-        String result = "";
-		try {
-			statement = connection.createStatement();
-	        ResultSet rs = statement.executeQuery("SELECT * FROM threatdata");
-	        while(rs.next()) {
-	        	int id = rs.getInt("id");
-	        	int threat = rs.getInt("threat");
-	        	result += "{" + id + ":" + threat + "} ";
-	        }
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+				   System.out.println("Connected to database!");
+				} catch (Exception e) {
+				   e.printStackTrace();
+				   System.err.println(e.getClass().getName()+": "+e.getMessage());
+				   System.err.println("FAILED TO CONNECT TO POSTGRES DATABASE");
+				   connection = null;
+				}
+			
+	   }
+   
+		public static String getAllData() {
+			if(connection == null) {
+				return "database error";
+			}
+		   Statement statement;
+		   String result = "";
+		   try {
+			   statement = connection.createStatement();
+			   ResultSet rs = statement.executeQuery("SELECT * FROM threatdata");
+			   while(rs.next()) {
+				   int id = rs.getInt("id");
+				   int threat = rs.getInt("threat");
+				   result += "{" + id + ":" + threat + "} ";
+			   }
+		   } catch (SQLException e) {
+			   // TODO Auto-generated catch block
+			   e.printStackTrace();
+		   }
+		   return result;
 		}
-		return result;
-	 }
-}
+   }
