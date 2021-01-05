c0df05c8464832715f76d6a5db8847c6e55b409f
OtisL <42128680+OtisL@users.noreply.github.com>
2021-01-04 18:26:08 -0800

    Started work on dial

--- src/main/resources/static/redirect.js
+++ src/main/resources/static/redirect.js
@@ -3,15 +3,39 @@ function showThreatLevel() {
 	let data = JSON.parse(myStorage.getItem("object"))
 	const threatLevel = data.threatLevel
 	const latestTweet = data.latestTweet
-	alert(data.message);
-
 	let threatLevelValue = document.getElementById("threatLevelTitle");
 	let latestTweetValue = document.getElementById("latestTweet");
 	threatLevelValue.innerHTML = "Your Threat Level is: " + threatLevel;
-	if(latestTweet!=null){
-		latestTweetValue.innerHTML = "Latest Tweet: "+latestTweet;
-	}else{
+	if (latestTweet != null) {
+		latestTweetValue.innerHTML = "Latest Tweet: " + latestTweet;
+	} else {
 		latestTweetValue.innerHTML = " ";
 	}
+	drawThreatMeter()
+}
+function drawThreatMeter() {
+	let canvas = document.getElementById("canvas");
+	let ctx = canvas.getContext("2d");
+	ctx.canvas.width = 400;
+	ctx.canvas.height = 200;
+	ctx.beginPath();
+	ctx.arc(200, 120, 100, Math.PI, 2 * Math.PI);
+	ctx.closePath();
+	ctx.beginPath();
+	ctx.arc(200, 190, 150, Math.PI, 2 * Math.PI);
+	//ctx.lineTo(95,50);
+	ctx.closePath();
+	var grd = ctx.createLinearGradient(60, 0, 400, 0);
+	grd.addColorStop(0, "red");
+	grd.addColorStop(0.25, "orange");
+	grd.addColorStop(0.55, "yellow");
+	grd.addColorStop(1, "green");
+	// Fill with gradient
+	ctx.fillStyle = grd;
+	ctx.fill();
+
+
+	ctx.stroke();
+
 }
 showThreatLevel()
