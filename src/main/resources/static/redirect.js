b67b7c0eb6d978c470aa5e19f93098956e5ca622
OtisL <42128680+OtisL@users.noreply.github.com>
2021-01-11 18:29:20 -0800

    Make animation work

--- src/main/resources/static/redirect.js
+++ src/main/resources/static/redirect.js
@@ -1,7 +1,9 @@
 const myStorage = window.localStorage
+let currentThreatLevel = 0
+let threatLevel = 0;
 function showThreatLevel() {
 	let data = JSON.parse(myStorage.getItem("object"))
-	const threatLevel = data.threatLevel
+	threatLevel = data.threatLevel
 	const latestTweet = data.latestTweet
 	let threatLevelValue = document.getElementById("threatLevelTitle");
 	let latestTweetValue = document.getElementById("latestTweet");
@@ -12,7 +14,7 @@ function showThreatLevel() {
 		latestTweetValue.innerHTML = "Latest Tweet: " + latestTweet;
 		
 	}
-	drawThreatMeter()
+	drawThreatMeter(threatLevel)
 }
 function drawThreatMeter() {
 	let canvas = document.getElementById("canvas");
@@ -34,17 +36,27 @@ function drawThreatMeter() {
 	ctx.fill();
 	let deg = Math.PI;
 	let start = Math.PI+Math.PI/2;
+	let radius = 150;
 	
-	let radius = 100;
 	let maxThreatLevel = 10;
-	let threatLevel = 2;
-	deg=start-threatLevel*Math.PI/maxThreatLevel
+	deg=start-currentThreatLevel*Math.PI/maxThreatLevel
 	ctx.beginPath();
 	ctx.moveTo(200,190)
 	ctx.lineTo(200+Math.sin(deg)*radius, 190+Math.cos(deg)*radius)
+	ctx.closePath();
+	ctx.strokeStyle = "black";
+	ctx.lineWidth = 10
+	ctx.stroke();
+	ctx.beginPath();
+	ctx.arc(200+Math.sin(deg)*radius, 190+Math.cos(deg)*radius, 5, 0, 2*Math.PI)
 	ctx.strokeStyle = "red";
 	ctx.lineWidth = 10
 	ctx.stroke();
-
+	currentThreatLevel+=0.07
+	if(currentThreatLevel < threatLevel){
+		window.requestAnimationFrame(drawThreatMeter)
+		
+	}
 }
+
 showThreatLevel()
