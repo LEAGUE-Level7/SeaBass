7ce1e788cc8019379c08c7af4d90476e3d2a3e79
OtisL <42128680+OtisL@users.noreply.github.com>
2021-01-11 18:00:42 -0800

    Working on line, not finished

--- src/main/resources/static/redirect.js
+++ src/main/resources/static/redirect.js
@@ -19,23 +19,31 @@ function drawThreatMeter() {
 	let ctx = canvas.getContext("2d");
 	ctx.canvas.width = 400;
 	ctx.canvas.height = 200;
-	ctx.beginPath();
-	ctx.arc(200, 120, 100, Math.PI, 2 * Math.PI);
-	ctx.closePath();
-	ctx.beginPath();
+	//ctx.beginPath();
+	//ctx.arc(200, 120, 100, Math.PI, 2 * Math.PI);
+	//ctx.closePath();
 	ctx.arc(200, 190, 150, Math.PI, 2 * Math.PI);
 	//ctx.lineTo(95,50);
-	ctx.closePath();
 	var grd = ctx.createLinearGradient(60, 0, 400, 0);
-	grd.addColorStop(0, "red");
-	grd.addColorStop(0.25, "orange");
-	grd.addColorStop(0.55, "yellow");
-	grd.addColorStop(1, "green");
+	grd.addColorStop(0, "green");
+	grd.addColorStop(0.25, "yellow");
+	grd.addColorStop(0.55, "orange");
+	grd.addColorStop(1, "red");
 	// Fill with gradient
 	ctx.fillStyle = grd;
 	ctx.fill();
-
-
+	let deg = Math.PI;
+	let start = Math.PI+Math.PI/2;
+	
+	let radius = 100;
+	let maxThreatLevel = 10;
+	let threatLevel = 2;
+	deg=start-threatLevel*Math.PI/maxThreatLevel
+	ctx.beginPath();
+	ctx.moveTo(200,190)
+	ctx.lineTo(200+sin(deg)*radius, 190+cos(deg)*radius)
+	ctx.strokeStyle = "red";
+	ctx.lineWidth = 10
 	ctx.stroke();
 
 }
