cefc004c68b1747a53abdd66ca76ae59c71d2c6b
OtisL <42128680+OtisL@users.noreply.github.com>
2021-02-01 17:32:00 -0800

    Added error stuff, removed the site dropdown.

--- src/main/java/com/keith/mydemo/ErrorPage.java
+++ src/main/java/com/keith/mydemo/ErrorPage.java
@@ -4,17 +4,29 @@ import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class ErrorPage implements ErrorController{
+public class ErrorPage implements ErrorController {
 	@RequestMapping("/error")
 	String errorMethod(HttpServletRequest request) {
 		Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-	
-		return "error.html?type=404";
+		if (status != null) {
+			Integer statusCode = Integer.valueOf(status.toString());
+			if (statusCode == HttpStatus.NOT_FOUND.value()) {
+				return "error404.html";
+			} else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+				return "error500.html";
+			} else if (statusCode == HttpStatus.FORBIDDEN.value()) {
+				return "error403.html";
+			} else if (statusCode == HttpStatus.SERVICE_UNAVAILABLE.value()) {
+				return "error503.html";
+			}
+		}
+		return "error.html";
 	}
 
 	@Override
